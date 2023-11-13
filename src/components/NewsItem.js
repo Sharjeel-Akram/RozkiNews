e4f26e4d30aa70583cf3fc0d1481a5c58b4ba61c
Sharjeel-Akram <sharjeel.akram0200@gmail.com>
2023-11-14 00:17:11 +0500

    card design has been updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
 // export class NewsItem extends Component {
-const NewsItem = (props) =>{
-    // render() {
+    const NewsItem = (props) => {
         let { title, description, imageUrl, newsUrl, author, publishedAt, source } = props;
         return (
             <div className='my-3'>
-                <div className="card">
-                    <div  style={{display: 'flex', justifyContent: 'flex-end', position:'absolute', right: '0'}}>
-                    <span className="badge rounded-pill bg-danger">{source}
-                    </span>
+                <div className="card" style={{ height: '100%' }}>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                        <span className="badge rounded-pill bg-danger">{source}</span>
                     </div>
-                    <img src={!imageUrl ? "https://www.coindesk.com/resizer/y4c1BLEK5yJGzg2KaKQS6J4Y7uE=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/IJ34EQNWQND5XOF67EUFS5ZSZ4.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <img 
+                        src={!imageUrl ? "default_image_url_here" : imageUrl} 
+                        className="card-img-top" 
+                        alt="..." 
+                        style={{ height: '200px', objectFit: 'cover' }} 
+                    />
                     <div className="card-body">
-                        <h6 className="card-title">{title}</h6>
-                        <p className="card-text">{description}...</p>
+                        <h6 className="card-title" style={{ height: '3em', overflow: 'hidden' }}>{title}</h6>
+                        <p className="card-text" style={{ height: '4.5em', overflow: 'hidden' }}>{description}...</p>
                         <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(publishedAt).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More..</a>
                     </div>
@@ -22,5 +25,6 @@ const NewsItem = (props) =>{
             </div>
         )
     }
-
-export default NewsItem
+    
+    export default NewsItem
+    
